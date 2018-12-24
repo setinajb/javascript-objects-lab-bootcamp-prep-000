d37e086ba55f0620310e3b6e4abb0567a9ab797b
Jaclyn Setina <jaclyn.setina@gmail.com>
2018-12-24 04:30:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-
+function deleteFromObjectByKey(object, key) {
+  var newObject = delete object[key];
+  return newObject
+}
